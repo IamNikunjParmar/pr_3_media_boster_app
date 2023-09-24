a4e79b57883b9fb6b0495bc83d950d695e2598e0
nikunjparmar21899 <prof.nikunjparmar5066@gmail.com>
2023-09-24 23:29:56 +0530

    news

---
+++ pr_3_media_boster_app/lib/views/screens/extra_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class ExtraPage extends StatelessWidget {
+  const ExtraPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
